cef4d6e0a999c78d99de80618d0613509b6828c7
CristianoSFMothe <cristianodevsystem@gmail.com>
2024-02-25 14:21:37 -0300

    :rocket: chore: usando o include nas páginas para o head

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var conn = require('./../inc/dabase');
+var conn = require("./../inc/dabase");
 var express = require("express");
 var router = express.Router();
 
@@ -15,6 +15,22 @@ router.get("/", function (req, res, next) {
       res.render("index", { title: "Restaurante Saboroso!", menus: results });
     }
   );
+});
+
+router.get("/contacts", (req, res, next) => {
+  res.render("contacts", { title: "Restaurante Saboroso!"});
+});
+
+router.get("/menus", (req, res, next) => {
+  res.render("menus", { title: "Restaurante Saboroso!"});
+});
+
+router.get("/reservations", (req, res, next) => {
+  res.render("reservations", { title: "Restaurante Saboroso!"});
+});
+
+router.get("/services", (req, res, next) => {
+  res.render("services", { title: "Restaurante Saboroso!"});
 });
 
 module.exports = router;
