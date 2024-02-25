17edae42b13c2a6b68e8b1c3c32ef28863650948
CristianoSFMothe <cristianodevsystem@gmail.com>
2024-02-25 13:06:37 -0300

    :rocket: feat: criando a conexão com o banco de dados na aplicação

---
+++ inc/dabase.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+require('dotenv').config();
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD
+});
+
+module.exports = connection;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
+var conn = require('./../inc/dabase');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  // Configuração de conexão com o Banco de Dados
+  conn.query('SELECT * FROM tb_users ORDER BY name', (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  })
 });
 
 module.exports = router;
